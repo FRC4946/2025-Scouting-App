19a682d942cc13e7249503cdf5066478b87014ba
RustyQuasar <dvwninja@outlook.com>
2025-01-11 10:04:35 -0500

    LOAD HALF WORKS YOOOOOOOOOO

--- app/src/main/java/com/example/a2024scoutingapp/Auto.java
+++ app/src/main/java/com/example/a2024scoutingapp/Auto.java
@@ -1,41 +1,46 @@
 package com.example.a2024scoutingapp;
 
-import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.example.a2024scoutingapp.forms.ScoutingForm;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Locale;
 
 public class Auto extends AppCompatActivity {
-    // TODO: redo like most of this
+    private static final String TAG = "AutoActivity";
+    private static final String DIRECTORY_NAME = "Logs";
+
     private ScoutingForm m_currentForm;
-    private Button  autoL4, autoL3, autoL2, autoL1, autoProcessor, autoNet, teleop, send, exit;
+    private Button autoL4, autoL3, autoL2, autoL1, autoProcessor, autoNet, teleop, send, exit;
     private CheckBox deleteMode, disabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.auto);
+
         m_currentForm = (ScoutingForm) getIntent().getSerializableExtra("SCOUTING_FORM");
+
         autoL4 = findViewById(R.id.autoL4Coral);
-        autoL4.setText("L4 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL4Coral)));;
+        autoL4.setText("L4 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL4Coral)));
         autoL3 = findViewById(R.id.autoL3Coral);
-        autoL3.setText("L3 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL3Coral)));;
+        autoL3.setText("L3 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL3Coral)));
         autoL2 = findViewById(R.id.autoL2Coral);
-        autoL2.setText("L2 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL2Coral)));;
+        autoL2.setText("L2 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL2Coral)));
         autoL1 = findViewById(R.id.autoL1Coral);
-        autoL1.setText("L1 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL1Coral)));;
+        autoL1.setText("L1 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL1Coral)));
         autoNet = findViewById(R.id.autoNet);
-        autoNet.setText("Algae Net: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoNet)));;
+        autoNet.setText("Algae Net: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoNet)));
         autoProcessor = findViewById(R.id.autoProcessor);
         autoProcessor.setText("Algae Processor: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoProcessor)));
         teleop = findViewById(R.id.teleop);
@@ -43,15 +48,16 @@ public class Auto extends AppCompatActivity {
         exit = findViewById(R.id.exitbutton2);
         disabled = findViewById(R.id.disabled);
         deleteMode = findViewById(R.id.delete);
-        m_currentForm = (ScoutingForm) getIntent().getSerializableExtra("SCOUTING_FORM");
+
         Button[] Buttons = {autoL4, autoL3, autoL2, autoL1, autoProcessor, autoNet};
-        for (int i = 0; i < Buttons.length; i++){
-            if (m_currentForm.team == Constants.Team.RED){
+        for (int i = 0; i < Buttons.length; i++) {
+            if (m_currentForm.team == Constants.Team.RED) {
                 Buttons[i].setBackgroundResource(R.drawable.redbutton);
             } else {
                 Buttons[i].setBackgroundResource(R.drawable.bluebutton);
             }
         }
+
         autoL4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,9 +66,10 @@ public class Auto extends AppCompatActivity {
                 } else {
                     m_currentForm.autoL4Coral++;
                 }
-                autoL4.setText("L4 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL4Coral)));;
+                autoL4.setText("L4 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL4Coral)));
             }
         });
+
         autoL3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,9 +78,10 @@ public class Auto extends AppCompatActivity {
                 } else {
                     m_currentForm.autoL3Coral++;
                 }
-                autoL3.setText("L3 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL3Coral)));;
+                autoL3.setText("L3 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL3Coral)));
             }
         });
+
         autoL2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,9 +90,10 @@ public class Auto extends AppCompatActivity {
                 } else {
                     m_currentForm.autoL2Coral++;
                 }
-                autoL2.setText("L2 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL2Coral)));;
+                autoL2.setText("L2 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL2Coral)));
             }
         });
+
         autoL1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,9 +102,10 @@ public class Auto extends AppCompatActivity {
                 } else {
                     m_currentForm.autoL1Coral++;
                 }
-                autoL1.setText("L1 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL1Coral)));;
+                autoL1.setText("L1 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoL1Coral)));
             }
         });
+
         autoNet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,6 +117,7 @@ public class Auto extends AppCompatActivity {
                 autoNet.setText("Algae Net: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoNet)));
             }
         });
+
         autoProcessor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,6 +129,7 @@ public class Auto extends AppCompatActivity {
                 autoProcessor.setText("Algae Processor: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.autoProcessor)));
             }
         });
+
         teleop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -126,23 +138,44 @@ public class Auto extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 m_currentForm.disabled = disabled.isChecked();
-                        Intent intent = new Intent(Auto.this, MainActivity.class);
+                saveFormToFile();
+                Intent intent = new Intent(Auto.this, MainActivity.class);
                 intent.putExtra("SCOUTING_FORM", m_currentForm);
-                ActivityCompat.requestPermissions(Auto.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Constants.WRITE_LOG_REQUEST);
                 startActivity(intent);
             }
         });
+
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 m_currentForm.disabled = disabled.isChecked();
                 Intent intent = new Intent(Auto.this, MainActivity.class);
-                intent.putExtra("SCOUTING_FORM", m_currentForm);startActivity(intent);
+                intent.putExtra("SCOUTING_FORM", m_currentForm);
+                startActivity(intent);
             }
         });
+    }
+
+    private void saveFormToFile() {
+        File logsDir = new File(getExternalFilesDir(null), DIRECTORY_NAME);
+        if (!logsDir.exists() && !logsDir.mkdirs()) {
+            Log.e(TAG, "Failed to create Logs directory.");
+            return;
+        }
+
+        File logFile = new File(logsDir, String.format(Locale.getDefault(),
+                "match-%d-team-%d.log", m_currentForm.matchNumber, m_currentForm.teamNumber));
+
+        try (FileWriter writer = new FileWriter(logFile)) {
+            writer.write(m_currentForm.toString());
+            Log.i(TAG, "Form saved successfully: " + logFile.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to save form", e);
+        }
     }
 }
--- app/src/main/java/com/example/a2024scoutingapp/Teleop.java
+++ app/src/main/java/com/example/a2024scoutingapp/Teleop.java
@@ -1,57 +1,63 @@
 package com.example.a2024scoutingapp;
 
-import android.Manifest;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.ToggleButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
 import com.example.a2024scoutingapp.forms.ScoutingForm;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Locale;
 
 public class Teleop extends AppCompatActivity {
-    // TODO: redo like most of this
+    private static final String TAG = "TeleopActivity";
+    private static final String DIRECTORY_NAME = "Logs";
+
     private ScoutingForm m_currentForm;
-    private Button  teleopL4, teleopL3, teleopL2, teleopL1, teleopProcessor, teleopNet, auto, send, exit;
+    private Button teleopL4, teleopL3, teleopL2, teleopL1, teleopProcessor, teleopNet, auto, send, exit;
     private CheckBox deleteMode, disabled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teleop);
+
         m_currentForm = (ScoutingForm) getIntent().getSerializableExtra("SCOUTING_FORM");
+
         teleopL4 = findViewById(R.id.teleopL4Coral);
-        teleopL4.setText("L4 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL4Coral)));;
+        teleopL4.setText("L4 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL4Coral)));
         teleopL3 = findViewById(R.id.teleopL3Coral);
-        teleopL3.setText("L3 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL3Coral)));;
+        teleopL3.setText("L3 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL3Coral)));
         teleopL2 = findViewById(R.id.teleopL2Coral);
-        teleopL2.setText("L2 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL2Coral)));;
+        teleopL2.setText("L2 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL2Coral)));
         teleopL1 = findViewById(R.id.teleopL1Coral);
-        teleopL1.setText("L1 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL1Coral)));;
+        teleopL1.setText("L1 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL1Coral)));
         teleopNet = findViewById(R.id.teleopNet);
-        teleopNet.setText("Algae Net: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopNet)));;
+        teleopNet.setText("Algae Net: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopNet)));
         teleopProcessor = findViewById(R.id.teleopProcessor);
         teleopProcessor.setText("Algae Processor: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopProcessor)));
-        exit = findViewById(R.id.exitbutton2);
         auto = findViewById(R.id.auto);
         send = findViewById(R.id.exitbutton);
+        exit = findViewById(R.id.exitbutton2);
         disabled = findViewById(R.id.disabled);
         deleteMode = findViewById(R.id.delete);
-        m_currentForm = (ScoutingForm) getIntent().getSerializableExtra("SCOUTING_FORM");
+
         Button[] Buttons = {teleopL4, teleopL3, teleopL2, teleopL1, teleopProcessor, teleopNet};
-        for (int i = 0; i < Buttons.length; i++){
-            if (m_currentForm.team == Constants.Team.RED){
+        for (int i = 0; i < Buttons.length; i++) {
+            if (m_currentForm.team == Constants.Team.RED) {
                 Buttons[i].setBackgroundResource(R.drawable.redbutton);
             } else {
                 Buttons[i].setBackgroundResource(R.drawable.bluebutton);
             }
         }
+
         teleopL4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,9 +66,10 @@ public class Teleop extends AppCompatActivity {
                 } else {
                     m_currentForm.teleopL4Coral++;
                 }
-                teleopL4.setText("L4 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL4Coral)));;
+                teleopL4.setText("L4 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL4Coral)));
             }
         });
+
         teleopL3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -71,9 +78,10 @@ public class Teleop extends AppCompatActivity {
                 } else {
                     m_currentForm.teleopL3Coral++;
                 }
-                teleopL3.setText("L3 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL3Coral)));;
+                teleopL3.setText("L3 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL3Coral)));
             }
         });
+
         teleopL2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,9 +90,10 @@ public class Teleop extends AppCompatActivity {
                 } else {
                     m_currentForm.teleopL2Coral++;
                 }
-                teleopL2.setText("L2 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL2Coral)));;
+                teleopL2.setText("L2 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL2Coral)));
             }
         });
+
         teleopL1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -93,9 +102,10 @@ public class Teleop extends AppCompatActivity {
                 } else {
                     m_currentForm.teleopL1Coral++;
                 }
-                teleopL1.setText("L1 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL1Coral)));;
+                teleopL1.setText("L1 Coral: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopL1Coral)));
             }
         });
+
         teleopNet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -107,6 +117,7 @@ public class Teleop extends AppCompatActivity {
                 teleopNet.setText("Algae Net: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopNet)));
             }
         });
+
         teleopProcessor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,6 +129,7 @@ public class Teleop extends AppCompatActivity {
                 teleopProcessor.setText("Algae Processor: " + String.format(Locale.getDefault(), Integer.toString(m_currentForm.teleopProcessor)));
             }
         });
+
         auto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -126,24 +138,44 @@ public class Teleop extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 m_currentForm.disabled = disabled.isChecked();
+                saveFormToFile();
                 Intent intent = new Intent(Teleop.this, MainActivity.class);
                 intent.putExtra("SCOUTING_FORM", m_currentForm);
-                ActivityCompat.requestPermissions(Teleop.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Constants.WRITE_LOG_REQUEST);
-                System.out.println("Saved");
                 startActivity(intent);
             }
         });
+
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 m_currentForm.disabled = disabled.isChecked();
                 Intent intent = new Intent(Teleop.this, MainActivity.class);
-                intent.putExtra("SCOUTING_FORM", m_currentForm);startActivity(intent);
+                intent.putExtra("SCOUTING_FORM", m_currentForm);
+                startActivity(intent);
             }
         });
+    }
+
+    private void saveFormToFile() {
+        File logsDir = new File(getExternalFilesDir(null), DIRECTORY_NAME);
+        if (!logsDir.exists() && !logsDir.mkdirs()) {
+            Log.e(TAG, "Failed to create Logs directory.");
+            return;
+        }
+
+        File logFile = new File(logsDir, String.format(Locale.getDefault(),
+                "match-%d-team-%d.log", m_currentForm.matchNumber, m_currentForm.teamNumber));
+
+        try (FileWriter writer = new FileWriter(logFile)) {
+            writer.write(m_currentForm.toString());
+            Log.i(TAG, "Form saved successfully: " + logFile.getAbsolutePath());
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to save form", e);
+        }
     }
 }
